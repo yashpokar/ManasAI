d8b828a2a375ab118ab44ccfc8664e3397e8025a
Yash Patel <hello@yashpokar.com>
2024-03-29 16:59:41 +0530

    designed chatbot form

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -1,10 +1,17 @@
 import typography from '@tailwindcss/typography'
+import form from '@tailwindcss/forms'
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        chat: ['Poppins', 'sans-serif']
+      },
+      rotate: {}
+    }
   },
-  plugins: [typography]
+  plugins: [typography, form]
 }
