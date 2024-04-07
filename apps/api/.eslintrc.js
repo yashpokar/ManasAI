9c2ddd5c3f8cc1f6fdaad0effcf7da541a49a78c
Yash Patel <hello@yashpokar.com>
2024-04-07 20:05:39 +0530

    cleaned up things

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,19 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -21,5 +24,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    'prettier/prettier': [
+      'error',
+      JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, '../..', '.prettierrc'), 'utf8')
+      )
+    ]
+  }
+}
