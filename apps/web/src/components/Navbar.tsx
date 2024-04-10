546e8f56bab1a7855ddcd742aa6459eed19d309c
Yash Patel <hello@yashpokar.com>
2024-04-11 00:14:46 +0530

    refactored the project selector dropdown

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import {
   SunIcon,
   MoonIcon,
@@ -6,7 +6,7 @@ import {
   PlusCircleIcon,
   CheckIcon
 } from '@heroicons/react/24/outline'
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 import { useTheme } from '../providers/ThemeProvider'
 import { useWorkspace } from '../providers/ProjectProvider'
@@ -15,34 +15,11 @@ import NewProject from './NewProject'
 const Navbar: React.FC = () => {
   const { isDarkMode, toggleTheme } = useTheme()
   const { workspaces, activeWorkspace, onWorkspaceChange } = useWorkspace()
-  const [workspaceSelectorOpen, openProjectsSelector] = useState(false)
   const [isNewProjectDialogOpen, openNewProjectDialog] = useState(
     workspaces.length === 0
   )
 
   const projectSelectorRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        projectSelectorRef.current &&
-        !projectSelectorRef.current.contains(event.target as Node)
-      ) {
-        openProjectsSelector(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [projectSelectorRef])
-
-  const onWorkspaceIdChange = (id: string) => {
-    onWorkspaceChange(id)
-    openProjectsSelector(false)
-  }
 
   return (
     <nav className="flex flex-col justify-between p-2 w-full h-full rounded-lg bg-zinc-200 dark:bg-zinc-800">
@@ -54,66 +31,78 @@ const Navbar: React.FC = () => {
       <ul className="">
         <li className="">
           <Popover className="relative text-center" ref={projectSelectorRef}>
-            <Popover.Button
-              className={clsx(
-                'p-0.5 md:p-1.5 rounded-full focus:outline-none hover:bg-zinc-300 dark:hover:bg-zinc-700',
-                {
-                  'bg-zinc-300 dark:bg-zinc-700': workspaceSelectorOpen
-                }
-              )}
-              onClick={() => openProjectsSelector(ps => !ps)}
-              data-testid="workspace-selector-button"
-            >
-              <QueueListIcon className="w-6 h-6 text-zinc-900 dark:text-zinc-50" />
-            </Popover.Button>
-
-            {workspaceSelectorOpen && (
-              <Popover.Panel
-                className="absolute z-10 right-12 -top-1 w-48 shadow-xl"
-                static
-              >
-                <div className="grid gap-y-1.5 p-2 rounded-lg bg-zinc-100 border-zinc-300 dark:bg-zinc-700 dark:border-zinc-800">
-                  {workspaces.map(({ id, name }) => (
-                    <button
-                      key={id}
-                      className={clsx(
-                        'flex justify-between py-2 px-4 text-left text-sm font-medium rounded-lg',
-                        {
-                          'bg-indigo-600 text-white hover:bg-indigo-700 dark:bg-indigo-600  hover:dark:bg-indigo-500':
-                            activeWorkspace?.id === id
-                        },
-                        {
-                          'text-zinc-600 dark:text-zinc-200 hover:bg-zinc-200 dark:hover:bg-zinc-600':
-                            activeWorkspace?.id !== id
-                        }
-                      )}
-                      onClick={() => onWorkspaceIdChange(id)}
-                      data-testid="workspace-selector-option"
-                    >
-                      {name}
-
-                      {activeWorkspace?.id === id && (
-                        <CheckIcon className="w-4 h-4" strokeWidth={2.5} />
-                      )}
-                    </button>
-                  ))}
-
-                  {workspaces.length > 0 && (
-                    <hr className="border-zinc-300 dark:border-zinc-800 opacity-45 my-1" />
+            {({ open, close }) => (
+              <>
+                <Popover.Button
+                  className={clsx(
+                    'p-0.5 md:p-1.5 rounded-full focus:outline-none hover:bg-zinc-300 dark:hover:bg-zinc-700',
+                    {
+                      'bg-zinc-300 dark:bg-zinc-700': open
+                    }
                   )}
+                  onClick={close}
+                  data-testid="workspace-selector-button"
+                >
+                  <QueueListIcon className="w-6 h-6 text-zinc-900 dark:text-zinc-50" />
+                </Popover.Button>
+                <Popover.Overlay className="fixed inset-0 bg-zinc-900 opacity-30" />
 
-                  <button
-                    className="py-2 text-xs font-semibold text-zinc-700 rounded-lg transition duration-75 dark:text-green-50 hover:text-zinc-50 hover:bg-green-500"
-                    onClick={() => {
-                      console.log('Add new workspace')
-                      openNewProjectDialog(true)
-                    }}
-                  >
-                    <PlusCircleIcon className="w-5 h-5 inline-block mr-2" />
-                    Add New
-                  </button>
-                </div>
-              </Popover.Panel>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-150"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute z-10 right-12 -top-1 w-48 shadow-xl">
+                    <div className="grid gap-y-1.5 p-2 rounded-lg bg-zinc-100 border-zinc-300 dark:bg-zinc-700 dark:border-zinc-800">
+                      {workspaces.map(({ id, name }) => (
+                        <button
+                          key={id}
+                          className={clsx(
+                            'flex justify-between py-2 px-4 text-left text-sm font-medium rounded-lg',
+                            {
+                              'bg-indigo-600 text-white hover:bg-indigo-700 dark:bg-indigo-600  hover:dark:bg-indigo-500':
+                                activeWorkspace?.id === id
+                            },
+                            {
+                              'text-zinc-600 dark:text-zinc-200 hover:bg-zinc-200 dark:hover:bg-zinc-600':
+                                activeWorkspace?.id !== id
+                            }
+                          )}
+                          onClick={() => {
+                            onWorkspaceChange(id)
+                            close()
+                          }}
+                          data-testid="workspace-selector-option"
+                        >
+                          {name}
+
+                          {activeWorkspace?.id === id && (
+                            <CheckIcon className="w-4 h-4" strokeWidth={2.5} />
+                          )}
+                        </button>
+                      ))}
+
+                      {workspaces.length > 0 && (
+                        <hr className="border-zinc-300 dark:border-zinc-800 opacity-45 my-1" />
+                      )}
+
+                      <button
+                        className="py-2 text-xs font-semibold text-zinc-700 rounded-lg transition duration-75 dark:text-green-50 hover:text-zinc-50 hover:bg-green-500"
+                        onClick={() => {
+                          openNewProjectDialog(true)
+                        }}
+                      >
+                        <PlusCircleIcon className="w-5 h-5 inline-block mr-2" />
+                        Add New
+                      </button>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
             )}
           </Popover>
         </li>
