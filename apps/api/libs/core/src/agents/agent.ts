4136971f24dc6dbaddaa58e6eb9e98c107bb3c40
Yash Patel <hello@yashpokar.com>
2024-04-17 23:39:15 +0530

    tested the planner

--- apps/api/libs/core/src/agents/agent.ts
+++ apps/api/libs/core/src/agents/agent.ts
@@ -1,14 +1,30 @@
 import { Message } from '@/models/message'
-import { Logger } from '@nestjs/common'
-import { Tool, toStructuredTools } from '../tools/tool'
+import { Injectable, Logger } from '@nestjs/common'
+import { toStructuredTools } from '../tools/tool'
 import { Tool as StructuredTool } from '@langchain/core/tools'
+import EditorTool from '../tools/editor'
+import TerminalTool from '../tools/terminal'
+import BrowserTool from '../tools/browser'
+import SearchTool from '../tools/search'
 
+@Injectable()
 abstract class Agent {
   protected logger = new Logger(this.constructor.name)
-  protected tools: StructuredTool[] = []
 
-  constructor(...tools: Tool[]) {
-    this.tools = toStructuredTools(tools)
+  constructor(
+    private readonly editor: EditorTool,
+    private readonly browser: BrowserTool,
+    private readonly terminal: TerminalTool,
+    private readonly search: SearchTool
+  ) {}
+
+  protected get tools(): StructuredTool[] {
+    return toStructuredTools([
+      this.editor,
+      this.browser,
+      this.terminal,
+      this.search
+    ])
   }
 
   abstract act(question: string, previousMessage: Message[]): Promise<void>
--- apps/api/libs/core/src/agents/openai.ts
+++ apps/api/libs/core/src/agents/openai.ts
@@ -1,27 +1,74 @@
 import { Injectable } from '@nestjs/common'
 import { pull } from 'langchain/hub'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { BasePromptTemplate, ChatPromptTemplate } from '@langchain/core/prompts'
 import { ChatOpenAI } from '@langchain/openai'
 import { createOpenAIFunctionsAgent } from 'langchain/agents'
 import { createAgentExecutor } from '@langchain/langgraph/prebuilt'
+import { JsonOutputFunctionsParser } from 'langchain/output_parsers'
+import { createOpenAIFnRunnable } from 'langchain/chains/openai_functions'
+import { END, StateGraph } from '@langchain/langgraph'
 
 import Agent from './agent'
-import EditorTool from '../tools/editor'
-import TerminalTool from '../tools/terminal'
-import BrowserTool from '../tools/browser'
-import SearchTool from '../tools/search'
+
+import { z } from 'zod'
+import { zodToJsonSchema } from 'zod-to-json-schema'
+
+const planExecuteState = {
+  input: {
+    value: null
+  },
+  plan: {
+    value: null,
+    default: () => []
+  },
+  pastSteps: {
+    value: (x, y) => x.concat(y),
+    default: () => []
+  },
+  response: {
+    value: null
+  }
+}
+
+const plan = zodToJsonSchema(
+  z.object({
+    steps: z
+      .array(z.string())
+      .describe('different steps to follow, should be in sorted order')
+  })
+)
+
+const planFunction = {
+  name: 'plan',
+  description: 'This tool is used to plan the steps to follow',
+  parameters: plan
+}
+
+const response = zodToJsonSchema(
+  z.object({
+    response: z.string().describe('Response to user.')
+  })
+)
+const responseFunction = {
+  name: 'response',
+  description: 'Response to user.',
+  parameters: response
+}
+
+type PlanExecuteState = {
+  input: string | null
+  plan: Array<string>
+  pastSteps: Array<string>
+  response: string | null
+}
+
+type ReplannerOutput = {
+  steps?: string[]
+  response?: string
+}
 
 @Injectable()
 class OpenAIAgent extends Agent {
-  constructor(
-    private readonly editor: EditorTool,
-    private readonly browser: BrowserTool,
-    private readonly terminal: TerminalTool,
-    private readonly search: SearchTool
-  ) {
-    super(editor, browser, terminal, search)
-  }
-
   async act(question: string): Promise<void> {
     const prompt = await pull<ChatPromptTemplate>(
       'hwchase17/openai-functions-agent'
@@ -40,11 +87,118 @@ class OpenAIAgent extends Agent {
       tools: this.tools
     })
 
-    const response = await agentExecutor.invoke({ input: question })
+    const plannerPrompt =
+      ChatPromptTemplate.fromTemplate(`For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
 
-    this.logger.log(
-      `OpenAIAgent acting on question: ${question}, response: ${JSON.stringify(response)}`
-    )
+{objective}`)
+    const model = new ChatOpenAI({
+      modelName: 'gpt-4-0125-preview'
+    }).bind({
+      functions: [planFunction],
+      function_call: planFunction
+    })
+    const parserSingle = new JsonOutputFunctionsParser({ argsOnly: true })
+    const planner = plannerPrompt.pipe(model).pipe(parserSingle)
+
+    const replannerPrompt =
+      ChatPromptTemplate.fromTemplate(`For the given objective, come up with a simple step by step plan.
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps.
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+Your objective was this:
+{input}
+
+Your original plan was this:
+{plan}
+
+You have currently done the follow steps:
+{pastSteps}
+
+Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that and use the 'response' function.
+Otherwise, fill out the plan.
+Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan.`)
+    const parser = new JsonOutputFunctionsParser()
+    const replanner = createOpenAIFnRunnable({
+      functions: [planFunction, responseFunction],
+      outputParser: parser,
+      llm: new ChatOpenAI({
+        modelName: 'gpt-4-0125-preview'
+      }),
+      prompt: replannerPrompt as BasePromptTemplate
+    })
+
+    async function executeStep(
+      state: PlanExecuteState
+    ): Promise<Partial<PlanExecuteState>> {
+      const task = state.input
+      const agentResponse = await agentExecutor.invoke({ input: task })
+      return {
+        pastSteps: [task, agentResponse.agentOutcome.returnValues.output]
+      }
+    }
+
+    async function planStep(
+      state: PlanExecuteState
+    ): Promise<Partial<PlanExecuteState>> {
+      const plan: ReplannerOutput = await planner.invoke({
+        objective: state.input
+      })
+
+      return { plan: plan.steps }
+    }
+
+    async function replanStep(
+      state: PlanExecuteState
+    ): Promise<Partial<PlanExecuteState>> {
+      const output: ReplannerOutput = await replanner.invoke({
+        input: state.input,
+        plan: state.plan ? state.plan.join('\n') : '',
+        pastSteps: state.pastSteps.join('\n')
+      })
+      if ('response' in output) {
+        return { response: output.response as string }
+      }
+
+      return { plan: output.steps }
+    }
+
+    function shouldEnd(state: PlanExecuteState) {
+      if (state.response) {
+        return 'true'
+      }
+      return 'false'
+    }
+
+    const workflow = new StateGraph({
+      channels: planExecuteState
+    })
+
+    workflow.addNode('planner', planStep)
+    workflow.addNode('agent', executeStep)
+    workflow.addNode('replan', replanStep)
+    workflow.setEntryPoint('planner')
+    workflow.addEdge('planner', 'agent')
+    workflow.addEdge('agent', 'replan')
+
+    workflow.addConditionalEdges('replan', shouldEnd, {
+      true: END,
+      false: 'planner'
+    })
+
+    const app = workflow.compile()
+
+    const config = { recursionLimit: 20 }
+    const inputs = {
+      input: question
+    }
+
+    for await (const event of await app.stream(inputs, config)) {
+      this.logger.log(`PLAN is ${JSON.stringify(event)}`)
+    }
+
+    this.logger.log(`OpenAIAgent acting on question: ${question}`)
   }
 }
 
